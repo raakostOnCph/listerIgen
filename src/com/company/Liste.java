23d4778bf16b1fa67e10f42421829c80e48ef78d
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2021-09-07 10:35:32 +0200

    insertFromGHead

--- src/com/company/Liste.java
+++ src/com/company/Liste.java
@@ -7,5 +7,28 @@ public class Liste
     Node tail = null;
 
 
+    public boolean isEmpty() {
+
+        if (head == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public Node insertFromHead(String s) {
+
+        Node node = new Node(s);
+
+        if (isEmpty()) {
+            head = node;
+            tail = node;
+            return head;
+        }
+        head.previous = node;
+        node.next = head;
+        head = node;
+        return head;
+
+    }
 
 }
