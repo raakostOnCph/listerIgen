209e644a40df77bfb81f245644981aaa393b4606
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2021-09-07 10:04:28 +0200

    har lavet nodeklassen

---
+++ src/com/company/Node.java
@@ -0,0 +1,15 @@
+package com.company;public
+
+
+class Node {
+
+    Node previous = null;
+    Node next = null;
+
+    String data;
+
+    public Node(String data)
+    {
+        this.data = data;
+    }
+}
